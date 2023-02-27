f8736a089520dbfbcfa2196b347874e676bd675d
Daniel Jackoway <commits@danieljackoway.com>
2023-02-27 10:24:45 -0800

    Hammerspoon: Move windows between screens

--- hammerspoon-init.lua
+++ hammerspoon-init.lua
@@ -97,6 +97,16 @@ hs.hotkey.bind(mash, 'left', function() hs.window.focusedWindow():move(units.lef
 hs.hotkey.bind(mash, 'right', function() hs.window.focusedWindow():move(units.right50,    nil, true) end)
 hs.hotkey.bind(mash, 'u', function() changeHeight(hs.window.focusedWindow(), -150) end)
 hs.hotkey.bind(mash, 'n', function() changeHeight(hs.window.focusedWindow(), 150) end)
+-- https://stackoverflow.com/a/58662204
+hs.hotkey.bind(mash, 's', function()
+  -- get the focused window
+  local win = hs.window.focusedWindow()
+  -- get the screen where the focused window is displayed, a.k.a. current screen
+  local screen = win:screen()
+  -- compute the unitRect of the focused window relative to the current screen
+  -- and move the window to the next screen setting the same unitRect
+  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+end)
 
 
 
